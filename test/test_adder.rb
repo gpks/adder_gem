2ade71c3589abd9db7d1bf6c46002386dc50a184
Małgorzata Ksionek <meksionek@gmail.com>
2016-12-17 10:13:44 +0100

    Add two more tests

--- test/test_adder.rb
+++ test/test_adder.rb
@@ -6,4 +6,15 @@ class AdderTest < Minitest::Test
     assert_equal 5,
       Adder[1,2,2]
   end
+
+  def test_adder_multiple_args
+    assert_equal 7198,
+      Adder[1,2,2,6,7,3,5,65,56,6,546,65,45,5,564,564,45,564,546,563,43,34,6,7,7,474,34,546,457,7,45,34,456,47,456,64,76,745]
+  end
+
+  def test_adder_error
+    assert_raises NoMethodError do
+      Adder[Object.new, Object.new]
+    end
+  end
 end
